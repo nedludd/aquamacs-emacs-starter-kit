dc96077d39308efedf1591b72a4d028b92eefb12
evangineer <mamading@gmail.com>
2010-02-22 11:00:03 +0000

    fixed to work with js2-mode.el instead of js2.el

--- js2-mode-setup.el
+++ js2-mode-setup.el
@@ -7,8 +7,13 @@
 ;; http://github.com/evangineer/aquamacs-emacs-starter-kit
 
 (add-to-list 'load-path 
-	(concat kitfiles-dir "/site-lisp" "/js2-mode"))
-(autoload 'js2-mode "js2" nil t)
+             (concat kitfiles-dir "/site-lisp" "/js2-mode"))
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; make js2-mode tooltips work
+;; see http://nflath.com/2009/09/flymake/
+(setq help-at-pt-timer-delay 0)
+(help-at-pt-set-timer)
 
 (provide 'js2-mode-setup)
